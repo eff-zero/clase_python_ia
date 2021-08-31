cca2774a29b60c64d27cb4b6d34ea1796eb0cc17
Jesus Hernandez <jhernand105@cuc.edu.co>
2021-08-30 20:17:23 -0500

    Tipos de datos, conversiones y operaciones

--- clase_uno.py
+++ clase_uno.py
@@ -8,7 +8,7 @@ Created on Mon Aug 30 18:55:29 2021
 
 print('Hello World')
 
-# Variables 
+# VARIABLES
 a = 3
 print(type(a))
 a = 'Hola'
@@ -16,3 +16,83 @@ print(type(a))
 a = 3.5
 print(type(a))
 
+
+# OPERACIONES
+# Suma
+a = 9
+b = 2
+c = 9 + 2
+print(c)
+
+# Resta
+a = 9
+b = 2
+c = 9 - 2
+print(c)
+
+# Multiplicacion
+a = 9
+b = 2
+c = 9 * 2
+print(c)
+
+# Division
+a = 9
+b = 2
+c = a / b
+print(c)
+
+a = 9
+b = 2
+c = a // b
+print(c)
+
+# Potencia
+a = 9
+b = 2
+c = a ** 2
+print(c)
+
+# Raiz
+a = 27
+c = a ** (1/3)
+print(c)
+
+# import math
+# raiz = 16
+# raizc = math.sqrt(raiz)
+
+
+# TIPOS DE DATOS 
+# String str
+a = "Hola Mundo"
+a = 'Hola Mundo'
+b = "I can't do it"
+c = 'Alias "Jesus"'
+
+# Entero int
+a = 5
+
+# Decimal float
+a = 5.6
+
+# Booleano bool
+x = True
+y = False
+
+
+# CONVERSIONES ENTRE TIPOS DE DATOS
+# Convertir de x a entero
+a = '3'
+y = int(a)
+print(type(y))
+
+# Convertir de x a string
+a = '3'
+y = str(a)
+print(type(y))
+
+# Concatenaciones
+a = 'Hola'
+b = 'Mundo'
+c = a + ' ' + b
