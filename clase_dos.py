9d0b95507eed305e71ea6b46aa827df2218774a6
Jesus Hernandez <jhernand105@cuc.edu.co>
2021-09-06 15:03:32 -0500

    Ejercicios condicionales

--- clase_dos.py
+++ clase_dos.py
@@ -47,3 +47,40 @@ print(True and (False and True) or False or (True or True))  # True
 # Jerarquía de operaciones booleanas
 # 1. Paréntesis y Llaves
 # 2. Tabla de verdad
+
+# Estructura
+x = 1
+if (x > 0):
+    print('1')
+else:
+    print('2')
+print('3')
+
+# HUA que dada la edad de una persona indique si es mayor o menor de edad.
+edad = int(input('Ingrese su edad: '))
+if (edad > 0):
+    if (edad >= 18):
+        print('Usted es mayor de edad')
+    else:
+        print('Usted es menor de edad')
+else:
+    print('Numero invalido')
+
+# HUA que donde se aprueba una asignatura con 3.0 y la máxima es 5.0.
+nota = float(input('Ingrese su nota: '))
+if (nota >= 0 and nota <= 5):
+    if (nota >= 3):
+        print('Usted ha aprobado')
+    else:
+        print('Usted ha reprobado')
+else:
+    print('Nota inválida')
+
+# HUA que dado un número n diga si es negativo, positivo o cero.
+n = float(input('Ingrese su número: '))
+if (n > 0):
+    print('Su número es positivo')
+elif (n < 0):
+    print('Su número es negativo')
+else:
+    print(f'Su número es {n}')
