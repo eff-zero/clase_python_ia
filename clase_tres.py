db686982a38df5eeb5b06cc8d38c6c0106c62335
Jesus Hernandez <jhernand105@cuc.edu.co>
2021-09-13 21:05:10 -0500

    Funciones y parametros

--- clase_tres.py
+++ clase_tres.py
@@ -10,6 +10,7 @@ Created on Mon Sep 13 19:27:29 2021
 
 # LISTAS O VECTORES
 # Tipos de dato mutable y ordenado
+a = []
 a = [1, 2, 3, 4]
 b = [2, True, 'Hola', 3.4]
 c = [2, [3, 4], ['Hola', 'Mundo'], [2.3, [2, 5, 7], 2.6]]
@@ -50,6 +51,7 @@ b = c[3][1][:2]
 
 # TUPLA
 # Tipo de dato INMUTABLE (Su valor no cambia en el tiempo) y ordenado
+a = ()
 a = (1, 2, 3, 4)
 print(a[1])
 b = (2, True, 'Hola', 3.4)
@@ -60,12 +62,14 @@ c = (2, [3, 4], ['Hola', 'Mundo'], [2.3, [2, 5, 7], 2.6])
 # SET (No es diccionario)
 # Tipo de dato mutable pero NO ordenado
 a = {1, 2, 3, 4}
+a = set()
 b = {2, True, 'Hola', 3.4}
-c = (2, [3, 4], ['Hola', 'Mundo'], [2.3, [2, 5, 7], 2.6])  # No permite vectores en su interior 
+c = (2, [3, 4], ['Hola', 'Mundo'], [2.3, [2, 5, 7], 2.6])  # No permite vector en su interior 
 4 in a
 
 # DICCIONARIO
 # Mutable y NO ordenado
+a = {}
 a = {'nombre': 'Jesús', 'apellido': 'Hernández'}
 a = {1: 'Jesús', 2: 'Hernández'}
 a['nombre']
@@ -81,3 +85,74 @@ for valor in a.items():
 
 for llave, valor in a.items():
     print(f'Llave: {llave}, Valor: {valor}')
+
+
+# Funciones
+def nombre_funcion():
+    pass
+
+
+def saludar():
+    print('Hola mundo')
+
+
+def saludar(nombre):
+    print(f'Hola {nombre}')
+
+
+# Python no prermite la SOBRECARGA de métodos
+# Parámetros opcionales
+def saludar(nombre='Mundo'):
+    print(f'Hola {nombre}')
+
+
+def saludar(nombre, apellido=""):
+    print(f'Hola {nombre} {apellido}')
+
+# No puedo tener un parámetro obligatorio después de un
+# parámetro opcional
+def saludar(nombre="", apellido):  # NO SIRVE
+    print(f'Hola {nombre} {apellido}')
+
+
+# Parámetros ilimitados
+def saludar(*nombres):
+    print(f'Hola {nombres}')
+
+
+def saludar(*nombres):
+    for nombre in nombres:
+        print(f'Hola {nombres}')
+
+
+def saludar(*nombres, apellido=""):
+    for nombre in nombres:
+        print(f'Hola {nombre}')
+    print(f'Apellido: {apellido}')
+
+
+def saludar(*nombres, apellido):
+    for nombre in nombres:
+        print(f'Hola {nombre}')
+    print(f'Apellido: {apellido}')
+
+
+def saludar(**nombres):  # Esto espera como parametro un {key:value}
+    print(nombres)
+
+
+def operaciones(a, b):
+    suma = a + b
+    resta = a - b
+    mult = a * b
+    div = a / b
+    return suma, resta, mult, div
+
+
+resultado = operaciones(4, 5)
+
+suma, res, mul, div = operaciones(4, 5)
+
+suma, _, _, div = operaciones(4, 5)  # Solo suma y división | Descomprimir
+
+# while(condición):
